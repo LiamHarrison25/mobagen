76b33f58533dc81250fb589bacedc6e0b486a209
Liam Harrison <liam.harrison@mymail.champlain.edu>
2023-09-15 14:32:55 -0400

    Worked on John Conway
    Worked on the John Conway

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,8 +3,57 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
 
+  //(0,0) is the top left of the grid
+
+  int i, j;
+  for(i = 0; i < world.SideSize(); i++)
+  {
+    for(j = 0; j < world.SideSize(); j++)
+    {
+      Point2D currentPoint = Point2D(i, j);
+      bool isAlive = world.Get(currentPoint);
+      int numNeighbors = CountNeighbors(world, currentPoint);
+
+      if(isAlive)
+      {
+        if (numNeighbors <= 1)
+        {
+          world.SetNext(currentPoint, false); //turns off the point
+        }
+        else if (numNeighbors >= 4)
+        {
+          world.SetNext(currentPoint, false); //turns off the point
+        }
+        else if(numNeighbors == 2 || numNeighbors == 3)
+        {
+          world.SetNext(currentPoint, true); //keeps the point on
+        }
+      }
+      else
+      {
+        if(numNeighbors == 3)
+        {
+          world.SetNext(currentPoint, true); //turns on the point
+        }
+      }
+    }
+  }
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
+int neighborCount = 0;
 
+  int i, j;
+  for (i = -1; i < 4; i++)
+  {
+    for(j = -1; j < 4; j++)
+    {
+      Point2D temp = Point2D(point.x + i, point.y + j);
+      if(j != 0 && i != 0 && world.Get(temp))
+      {
+        neighborCount++;
+      }
+    }
+  }
+  return neighborCount;
 }
