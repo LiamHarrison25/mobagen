0d3e3d0ee4da496594f9f2a62f8c3475aa3f2d8c
Liam Harrison <liam.harrison@mymail.champlain.edu>
2023-10-09 14:09:59 -0400

    Worked on the cat movement
    Worked on most of the algorithm for the movement. Just need to make a way to check where points came from and then figure out which is the closest end point.

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -1,24 +1,119 @@
 #include "Cat.h"
 #include "World.h"
+#include <queue>
 #include <stdexcept>
+#include <map>
 
 Point2D Cat::Move(World* world) {
-  auto rand = Random::Range(0, 5);
-  auto pos = world->getCat();
-  switch (rand) {
-    case 0:
-      return World::NE(pos);
-    case 1:
-      return World::NW(pos);
-    case 2:
-      return World::E(pos);
-    case 3:
-      return World::W(pos);
-    case 4:
-      return World::SW(pos);
-    case 5:
-      return World::SE(pos);
-    default:
-      throw "random out of range";
+
+  Point2D catPos = world->getCat();
+
+  //Get the closest exit position
+
+  Point2D exitPoint;
+  int i, j;
+  for(i = -(world->getWorldSideSize() / 2); i < world->getWorldSideSize() / 2; i++)
+  {
+    for(j = -(world->getWorldSideSize() / 2); j < world->getWorldSideSize() / 2; j++)
+    {
+      //go through every point and detect if it is an end point
+      //world->catWinsOnSpace()
+
+    }
   }
+
+  //Loop until it finds a point to move to
+
+  std::queue<Point2D> frontier;
+  std::vector<Point2D> reached;
+  std::map<Point2D, Point2D> cameFrom;
+
+  //TODO: insert the catPos into the cameFrom map for all of these
+
+  frontier.push(world->E(catPos));
+
+  frontier.push(world->SE(catPos));
+
+  frontier.push(world->SW(catPos));
+
+  frontier.push(world->W(catPos));
+
+  frontier.push(world->NW(catPos));
+
+  frontier.push(world->NE(catPos));
+
+  while(!frontier.empty())
+  {
+    //Gets the neighbors:
+    Point2D currentPos = frontier.back();
+    frontier.pop();
+
+    if(currentPos == exitPoint)
+    {
+      break;
+    }
+    //TODO: Insert all of these into the cameFrom map: (world->X(currentPos), currentPos) // x: the direction
+
+    if(world->catCanMoveToPosition(world->E(currentPos)) && pointNotVisited(reached,world->E(currentPos)))
+    {
+      frontier.push(world->E(currentPos));
+      reached.push_back(world->E(currentPos));
+    }
+    if(world->catCanMoveToPosition(world->SE(currentPos)) && pointNotVisited(reached,world->SE(currentPos)))
+    {
+      frontier.push(world->SE(currentPos));
+      reached.push_back(world->SE(currentPos));
+    }
+
+    if(world->catCanMoveToPosition(world->SW(currentPos)) && pointNotVisited(reached,world->SW(currentPos)))
+    {
+      frontier.push(world->SW(currentPos));
+      reached.push_back(world->SW(currentPos));
+    }
+
+    if(world->catCanMoveToPosition(world->W(currentPos)) && pointNotVisited(reached,world->W(currentPos)))
+    {
+      frontier.push(world->W(currentPos));
+      reached.push_back(world->W(currentPos));
+    }
+
+    if(world->catCanMoveToPosition(world->NW(currentPos)) && pointNotVisited(reached,world->NW(currentPos)))
+    {
+      frontier.push(world->NW(currentPos));
+      reached.push_back(world->NW(currentPos));
+    }
+
+    if(world->catCanMoveToPosition(world->NE(currentPos)) && pointNotVisited(reached,world->NE(currentPos)))
+    {
+      frontier.push(world->NE(catPos));
+      reached.push_back(world->NE(currentPos));
+    }
+
+
+  }
+
+  //Gets the point to move towards
+  Point2D currentPos = exitPoint;
+  while(currentPos != catPos)
+  {
+    //TODO: set the currentPos to the position where the exit point came from
+    //currentPos = cameFrom.
+  }
+
+  return currentPos;
+
+}
+
+bool Cat::pointNotVisited(const std::vector<Point2D>& reached, const Point2D neighbor)
+{
+  int j;
+  for(j = 0; j < reached.size(); j++)
+  {
+    if(neighbor == reached[j])
+    {
+      return false;
+    }
+  }
+
+  return true; //returns that this is a new point
 }
--- examples/catchthecat/Cat.h
+++ examples/catchthecat/Cat.h
@@ -2,11 +2,13 @@
 #define CAT_H
 
 #include "Agent.h"
+#include <vector>
 
 class Cat : public Agent {
 public:
   explicit Cat() : Agent(){};
   Point2D Move(World*) override;
+  bool pointNotVisited(const std::vector<Point2D>& reached, const Point2D neighbor);
 };
 
 #endif  // CAT_H
